1a5b9f6f77b20f74e5be378a07f00ba391df79cd
Davidvl <josuedavidvl18@gmail.com>
2023-07-29 12:56:02 -0600

    creando las preguntas del quiz

---
+++ src/components/Quiz.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <question
+            v-if="preguntas[preguntaActual]"
+            :pregunta="preguntas[preguntaActual].pregunta"
+            :opciones="preguntas[preguntaActual].opciones"
+            @respuesta-seleccionada="responder"
+        />
+        <button @click="siguientePregunta" v-if="preguntaActual < preguntas.length - 1">Siguiente</button>
+        <p v-else>¡Has terminado el Quiz!</p>
+    </div>
+</template>
+
+
+<script>
+import Question from './Question.vue';
+
+export default {
+    components:  {  Question },
+    data() {
+        return {
+            preguntaActual: 0,
+            preguntas: [
+
+            ],
+        }
+    }
+}
+</script>
