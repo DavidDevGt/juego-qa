51be3fd22d6a324aefb9975d67b497161bf36de2
Davidvl <josuedavidvl18@gmail.com>
2023-07-29 12:46:21 -0600

    Creando componentes para el quiz

---
+++ src/components/Question.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>{{ pregunta }}</h2>
+        <div v-for="(opcion, index) in opciones" :key="index">
+            <input type="radio" :value="opcion" v-model="respuestaSeleccionada" :id="'opcion' + index">
+            <label :for="'opcion' + index">{{ opcion }}</label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['pregunta', 'opciones'],
+    data() {
+        return {
+            respuestaSeleccionada: null
+        };
+    },
+    watch: {
+        respuestaSeleccionada(nuevaRespuesta) {
+            this.$emit('respuesta-seleccionada', nuevaRespuesta);
+        }
+    }
+}
+</script>
